c32c8fd769fefbd5a7f3da11ad414434ca9f422b
sajid360 <ahteshamsajid8@gmail.com>
2023-10-05 20:44:06 +0600

    firebase deployed

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,15 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDV_q8bcfWNjTocHjkUYcqPAW0WmDHiAt4",
-  authDomain: "react-dragon-news-adbe9.firebaseapp.com",
-  projectId: "react-dragon-news-adbe9",
-  storageBucket: "react-dragon-news-adbe9.appspot.com",
-  messagingSenderId: "499527795768",
-  appId: "1:499527795768:web:074d697e764ce2bc0a12cd"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
